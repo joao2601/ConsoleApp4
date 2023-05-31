e9939a1ea81803aa0a7b87e7bfdaa1c4467357a1
CJ3010406 <CJ3010406@LAB01-PC41>
2023-05-31 13:04:44 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXERCICIO1
+            //int[] vet2;
+            //float media = 0;
+            //Console.WriteLine("entre com um tamanho");
+            //int T = int.Parse(Console.ReadLine());
+
+            //vet2 = new int[T];
+            //Random rand = new Random();
+
+
+            //for (int i = 0; i < T; i++)
+            //{
+            //    vet2[i] = rand.Next(10, 50);
+            //    media = media + vet2[i];
+            //}
+            //for (int i = 0; i < T; i++)
+            //{
+            //    Console.WriteLine("posição[{0}] = {1}", i, vet2[i]);
+            //}
+            //Console.WriteLine(media / (float)T);
+            //Console.ReadKey();
+
+
+
+
+            //EXERCICIO2
+            //int[] vet2;
+            //float media = 0, numPar = 0, numImpar = 0;
+            //Console.WriteLine("entre com um tamanho");
+            //int T = int.Parse(Console.ReadLine());
+
+            //vet2 = new int[T];
+            //Random rand = new Random();
+
+
+            //for (int i = 0; i < T; i++)
+            //{
+            //    vet2[i] = rand.Next(10, 50);
+
+            //    if (vet2[i] % 2 == 0)
+            //    {
+            //        numPar = numPar + vet2[i];
+            //    }
+            //    else
+            //        numImpar = numImpar + vet2[i];
+
+            //}
+            //for (int i = 0; i < T; i++)
+            //{
+            //    Console.WriteLine("posição[{0}] = {1}", i, vet2[i]);
+            //}
+            //Console.WriteLine("Os numeros pares são " + numPar + "os numeros impares são " + numImpar);
+            //Console.ReadKey();
+
+
+
+            //EXERCICIO3
+            //int[] vet2;
+            //float media = 0;
+            //Console.WriteLine("entre com um tamanho");
+            //int T = int.Parse(Console.ReadLine());
+
+            //vet2 = new int[T];
+            //Random rand = new Random();
+
+
+            //for (int i = 0; i < T; i++)
+            //{
+            //    vet2[i] = rand.Next(10, 50);
+            //    Console.WriteLine("posição[{0}] = {1}", i, vet2[i]);
+            //}
+
+
+            //for (int i = T-1; i >= 0; i--)
+            //{
+            //    Console.WriteLine("posição[{0}] = {1}", i, vet2[i]  );
+            //}
+
+            //Console.ReadKey();
+
+            //EXERCICIO4
+
+
+            int[] vet2;
+            float media = 0, numPar = 0, numImpar = 0;
+            Console.WriteLine("entre com um tamanho");
+            int T = int.Parse(Console.ReadLine());
+
+            vet2 = new int[T];
+            Random rand = new Random();
+            int cont = 0;
+
+            for (int i = 0; i < T; i++)
+            {
+                vet2[i] = rand.Next(10, 50);
+
+                if (vet2[i] % 3 == 0 && vet2[i] % 5 == 0)
+                {
+                    numPar = numPar + vet2[i];
+                    cont++;
+                }
+              
+            }
+            for (int i = 0; i < T; i++)
+            {
+                Console.WriteLine("posição[{0}] = {1}", i, vet2[i]);
+            }
+            Console.WriteLine("Os numeros pares são " + numPar /(float)cont);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
